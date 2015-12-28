1592f654c8db3c1c1ce720884e703ae7ec84d129
axel22 <aleksandar.prokopec@gmail.com>
2015-12-28 09:42:30 +0100

    Make `try-catch` tests more robust.

--- src/test/scala/scala/coroutines/try-catch-tests.scala
+++ src/test/scala/scala/coroutines/try-catch-tests.scala
@@ -8,7 +8,7 @@ import scala.util.Failure
 
 
 class TryCatchTest extends FunSuite with Matchers {
-  test("should declare a coroutine with a try-catch block") {
+  test("coroutine with a try-catch block") {
     val rube = coroutine { () =>
       try {
         throw new Exception
@@ -16,18 +16,56 @@ class TryCatchTest extends FunSuite with Matchers {
         case e: Exception =>
       }
     }
+
+    val c0 = call(rube())
+    assert(c0() == (()))
+    assert(c0.isStopped)
   }
 
-  test("should declare a coroutine with a try-catch-finally block") {
+  test("coroutine with a try-catch-finally block") {
     val rube = coroutine { () =>
       try {
-        throw new Exception
+        throw new Error
       } catch {
-        case e: Exception =>
+        case e: Error =>
       } finally {
         sys.error("done")
       }
     }
+
+    val c0 = call(rube())
+    c0.tryGet() match {
+      case Failure(re: RuntimeException) => assert(re.getMessage == "done")
+      case _ => assert(false)
+    }
+  }
+
+  test("coroutine with a try-catch-finally and several exception types") {
+    var completed = false
+    var runtime = false
+    var error = false
+    val rube = coroutine { (t: Throwable) =>
+      try {
+        throw t
+      } catch {
+        case e: RuntimeException =>
+          runtime = true
+        case e: Error =>
+          error = true
+      } finally {
+        completed = true
+      }
+    }
+
+    val c0 = call(rube(new Error))
+    assert(!runtime)
+    assert(!error)
+    assert(!completed)
+    assert(c0() == (()))
+    assert(!runtime)
+    assert(error)
+    assert(completed)
+    assert(c0.isStopped)
   }
 
   // test("should declare a coroutine with a throw statement") {
