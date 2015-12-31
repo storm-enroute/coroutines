1619e136d46516bf63b0413a898b0c3ecd96ad4f
axel22 <aleksandar.prokopec@gmail.com>
2015-12-31 13:25:44 +0100

    Add identity example.

---
+++ src/test/scala/scala/examples/Identity.scala
@@ -0,0 +1,15 @@
+package scala.examples
+
+
+import scala.coroutines._
+
+
+
+object Identity {
+  val id = coroutine { (x: Int) => x }
+
+  def main(args: Array[String]) {
+    val c = call(id(7))
+    assert(c() == 7)
+  }
+}
---
+++ src/test/scala/scala/examples/examples-tests.scala
@@ -0,0 +1,13 @@
+package scala.examples
+
+
+
+import org.scalatest._
+import scala.util.Failure
+
+
+
+class ExamplesTest extends FunSuite with Matchers {
+  test("identity coroutine") {
+  }
+}
