febda68eb29556693a48e22975d4b44d7743f04b
axel22 <aleksandar.prokopec@gmail.com>
2016-01-02 17:21:46 +0100

    Complete lifecycle example.

--- src/test/scala/scala/examples/Lifecycle.scala
+++ src/test/scala/scala/examples/Lifecycle.scala
@@ -2,24 +2,48 @@ package scala.examples
 
 
 
+import scala.collection._
 import scala.coroutines._
+import scala.util.Success
 
 
 
 object Lifecycle {
   val katamari: Int ~~> (String, Int) = coroutine { (n: Int) =>
     var i = 1
-    yieldval("naaaaaa")
+    yieldval("naaaa")
     while (i < n) {
       yieldval("na")
       i += 1
     }
     yieldval("Katamari Damacy!")
-    i
+    i + 2
   }
 
   def main(args: Array[String]) {
-    val c = call(katamari(5))
+    val c = call(katamari(9))
     assert(c.resume)
+    assert(c.hasValue)
+    assert(c.value == "naaaa")
+    for (i <- 1 until 9) {
+      assert(c.resume)
+      assert(c.getValue == Some("na"))
+    }
+    assert(c.resume)
+    assert(c.tryValue == Success("Katamari Damacy!"))
+    assert(!c.resume)
+    assert(c.getValue == None)
+    assert(c.result == 11)
+    assert(c.isCompleted)
+    assert(!c.isLive)
+
+    val theme = "naaaa na na na na na na na na Katamari Damacy!"
+    assert(drain(call(katamari(9))) == theme)
+  }
+
+  def drain(f: String <~> Int): String = {
+    val buffer = mutable.Buffer[String]()
+    while (f.resume) buffer += f.value
+    buffer.mkString(" ")
   }
 }
