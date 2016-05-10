83983cdd5c5c6996d3b311a9c8db1f08e703e304
axel22 <aleksandar.prokopec@gmail.com>
2016-05-10 03:26:50 +0200

    Add coroutine-based backtracking tests.

--- src/bench/scala/org/coroutines/ScalaCheckBench.scala
+++ src/bench/scala/org/coroutines/ScalaCheckBench.scala
@@ -65,4 +65,56 @@ class ScalaCheckBench extends JBench.OfflineReport {
     }
   }
 
+  class Backtracker {
+    val random = new Random
+
+    val recurse: (Unit <~> Unit) ~~> (Unit, Unit) = coroutine { (c: Unit <~> Unit) =>
+      if (c.resume) {
+        val saved = c.snapshot
+        recurse(c)
+        recurse(saved)
+      } else {
+        yieldval(())
+      }
+    }
+
+    val traverse = coroutine { (snippet: ~~~>[Unit, Unit]) =>
+      while (true) {
+        val c = call(snippet())
+        recurse(c)
+      }
+    }
+
+    def backtrack(snippet: ~~~>[Unit, Unit], numTests: Int): Unit = {
+      var testsLeft = numTests
+      val t = call(traverse(snippet))
+      for (i <- 0 until numTests) t.resume
+    }
+
+    val int = coroutine { (from: Int, until: Int) =>
+      yieldval(())
+      from + random.nextInt(until - from)
+    }
+  }
+
+  @gen("sizes")
+  @benchmark("coroutines.scalacheck.fractions")
+  @curve("coroutine")
+  def coroutineTestFraction(numTests: Int) = {
+    val b = new Backtracker
+    val fract = coroutine { () =>
+      val den = b.int(1, max)
+      val num = b.int(0, den)
+      Fract(num, den)
+    }
+    var i = 0
+    val test = coroutine { () =>
+      val a = fract()
+      val b = fract()
+      val c = add(a, b)
+      i += 1
+      assert(c.num < 2 * c.den)
+    }
+    b.backtrack(test, numTests)
+  }
 }
