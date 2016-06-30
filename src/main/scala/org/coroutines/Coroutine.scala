f583ff7bcf23ba37f1cf39c60fe8a8cdb20651b3
Jess smith <smith.jessk@gmail.com>
2016-06-30 18:02:19 -0400

    Document foreach, map. Add test

--- src/main/scala/org/coroutines/Coroutine.scala
+++ src/main/scala/org/coroutines/Coroutine.scala
@@ -301,17 +301,34 @@ object Coroutine {
       s">"
     }
 
+    /** Apply `f` to each value yielded by the coroutine.
+     *
+     *  Exceptions seen when in calls to `tryValue` are only thrown if `hasException`.
+     *  This is so nothing fails if the coroutine pauses but does not yield a value. For
+     *  instance, this avoids exceptions from `yieldto` points.
+     *
+     *  @param f The function to apply to each value yielded by the coroutine
+     */
     def foreach(f: (Y) => Unit) {
       while (resume) {
         tryValue match {
           case Success(value) => f(value)
-          case Failure(exception) => throw exception
+          case Failure(exception) =>
+            if (hasException) throw exception
         }
       }
     }
 
-    def map(f: (Y) => Y) = {
-      var resultList = immutable.Seq.empty[Y]
+    /** Apply `f` to each value yielded by the coroutine, and collect the results into
+     *  a `immutable.List[Z]`.
+     *
+     *  Internally, this calls `foreach`.
+     *
+     *  @param  `f` The function to apply to each value yielded by the coroutine
+     *  @return A `List` holding the results of the applications of `f`.
+     */
+    def map[@specialized Z](f: (Y) => Z): immutable.List[Z] = {
+      var resultList = immutable.Seq.empty[Z]
       foreach { element =>
         resultList = resultList :+ f(element)
       }
