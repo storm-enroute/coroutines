196424f1a36824f5c82fb0d3d0b8f92547c84d66
Jess Smith <smith.jessk@gmail.com>
2016-07-29 15:22:14 -0400

    Add Enumerator.apply. Remove old code

--- coroutines-extra/src/main/scala/org/coroutines/extra/Enumerators.scala
+++ coroutines-extra/src/main/scala/org/coroutines/extra/Enumerators.scala
@@ -7,25 +7,11 @@ import scala.collection._
 
 
 
-// `Y` is the member type of the enumerator.
-trait Enumerator[@specialized Y] {
-  /** Apply `f` to each value yielded by the coroutine.
-   *
-   *  Exceptions seen when in calls to `tryValue` are only thrown if `hasException`.
-   *  This is so nothing fails if the coroutine pauses but does not yield a value. For
-   *  instance, this avoids exceptions from `yieldto` points.
-   *
-   *  @param f The function to apply to each value yielded by the coroutine
-   */
-  def foreach(f: (Y) => Unit)
+class Enumerator[@specialized Y](c: Coroutine.Instance[Y, _]) {
+}
 
-  /** Apply `f` to each value yielded by the coroutine, and collect the results into
-   *  a `immutable.List[Z]`.
-   *
-   *  Internally, this calls `foreach`.
-   *
-   *  @param  `f` The function to apply to each value yielded by the coroutine
-   *  @return A `List` holding the results of the applications of `f`.
-   */
-  def map[@specialized Z](f: (Y) => Z): immutable.List[Z]
+object Enumerator {
+  def apply[Y](c: Coroutine.Instance[Y, _]) = new Enumerator(c)
+
+  def apply[Y](c: Coroutine._0[Y, _]) = new Enumerator(call(c()))
 }
