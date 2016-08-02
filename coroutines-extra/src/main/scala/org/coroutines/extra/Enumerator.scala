79d0a6c6b29eda3050214fb56057ffab770285bf
Jess Smith <smith.jessk@gmail.com>
2016-08-02 19:18:19 -0400

    Take snapshot of `Coroutine.Instance` inside `Enumerator.apply`

--- coroutines-extra/src/main/scala/org/coroutines/extra/Enumerator.scala
+++ coroutines-extra/src/main/scala/org/coroutines/extra/Enumerator.scala
@@ -39,7 +39,7 @@ class Enumerator[@specialized Y](instance: Coroutine.Instance[Y, _]) {
 }
 
 object Enumerator {
-  def apply[Y](c: Coroutine.Instance[Y, _]) = new Enumerator(c)
+  def apply[Y](c: Coroutine.Instance[Y, _]) = new Enumerator(c.snapshot)
 
   def apply[Y](c: Coroutine._0[Y, _]) = new Enumerator(call(c()))
 }
