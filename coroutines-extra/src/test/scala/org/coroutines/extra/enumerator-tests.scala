653396857b2b5af10393167576137b466eeabf76
Jess Smith <smith.jessk@gmail.com>
2016-08-02 19:19:02 -0400

    Test creation of multiple enumerators from same `Coroutine._0`, `Coroutine.Instance`.

--- coroutines-extra/src/test/scala/org/coroutines/extra/enumerator-tests.scala
+++ coroutines-extra/src/test/scala/org/coroutines/extra/enumerator-tests.scala
@@ -15,23 +15,42 @@ class EnumeratorsTest extends FunSuite with Matchers {
     yieldval(3)
   }
 
+  // Asserts that `apply` takes a `snapshot` of the instance.
   test("enumerator creation from coroutine instance") {
     val instance = call(rube())
-    val enumerator = Enumerator(instance)
-    assert(enumerator.hasNext())
-    assert(enumerator.next == 1)
-    assert(enumerator.next == 2)
-    assert(enumerator.next == 3)
-    assert(!enumerator.hasNext)
+
+    val enumerator1 = Enumerator(instance)
+    assert(enumerator1.hasNext())
+    assert(enumerator1.next == 1)
+    assert(enumerator1.next == 2)
+    assert(enumerator1.next == 3)
+    assert(!enumerator1.hasNext)
+
+    val enumerator2 = Enumerator(instance)
+    assert(enumerator2.hasNext())
+    assert(enumerator2.next == 1)
+    assert(enumerator2.next == 2)
+    assert(enumerator2.next == 3)
+    assert(!enumerator2.hasNext)
   }
 
+  /** Asserts that more than one `Enumerator` can be created from the same
+   *  `Coroutine._0`.
+   */
   test("enumerator creation from coroutine_0") {
-    val enumerator = Enumerator(rube)
-    assert(enumerator.hasNext())
-    assert(enumerator.next == 1)
-    assert(enumerator.next == 2)
-    assert(enumerator.next == 3)
-    assert(!enumerator.hasNext)
+    val enumerator1 = Enumerator(rube)
+    assert(enumerator1.hasNext())
+    assert(enumerator1.next == 1)
+    assert(enumerator1.next == 2)
+    assert(enumerator1.next == 3)
+    assert(!enumerator1.hasNext)
+
+    val enumerator2 = Enumerator(rube)
+    assert(enumerator2.hasNext())
+    assert(enumerator2.next == 1)
+    assert(enumerator2.next == 2)
+    assert(enumerator2.next == 3)
+    assert(!enumerator2.hasNext)
   }
 
   test("enumerator should ignore return value of coroutine") {
