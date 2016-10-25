adddd67c3925b362d271b0a4c834ffac760ce9b9
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2016-10-25 22:51:39 +0200

    Add sbt-idea plugin, remove old-style settings.

--- project/Build.scala
+++ project/Build.scala
@@ -31,64 +31,63 @@ object CoroutinesBuild extends MechaRepoBuild {
     coroutinesCrossScalaVersions.value.head
   }
 
-  val coroutinesSettings =
-    Defaults.defaultSettings ++ MechaRepoPlugin.defaultSettings ++ Seq(
-      name := "coroutines",
-      organization := "com.storm-enroute",
-      version <<= frameworkVersion,
-      scalaVersion <<= coroutinesScalaVersion,
-      crossScalaVersions <<= coroutinesCrossScalaVersions,
-      libraryDependencies <++= (scalaVersion)(sv => dependencies(sv)),
-      libraryDependencies ++= superRepoDependencies("coroutines"),
-      testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
-      scalacOptions ++= Seq(
-        "-deprecation",
-        "-unchecked",
-        "-optimise",
-        "-Yinline-warnings"
-      ),
-      resolvers ++= Seq(
-        "Sonatype OSS Snapshots" at
-          "https://oss.sonatype.org/content/repositories/snapshots",
-        "Sonatype OSS Releases" at
-          "https://oss.sonatype.org/content/repositories/releases"
-      ),
-      ivyLoggingLevel in ThisBuild := UpdateLogging.Quiet,
-      publishMavenStyle := true,
-      publishTo <<= version { (v: String) =>
-        val nexus = "https://oss.sonatype.org/"
-        if (v.trim.endsWith("SNAPSHOT"))
-          Some("snapshots" at nexus + "content/repositories/snapshots")
-        else
-          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
-      },
-      publishArtifact in Test := false,
-      pomIncludeRepository := { _ => false },
-      pomExtra :=
-        <url>http://storm-enroute.com/</url>
-        <licenses>
-          <license>
-            <name>BSD-style</name>
-            <url>http://opensource.org/licenses/BSD-3-Clause</url>
-            <distribution>repo</distribution>
-          </license>
-        </licenses>
-        <scm>
-          <url>git@github.com:storm-enroute/coroutines.git</url>
-          <connection>scm:git:git@github.com:storm-enroute/coroutines.git</connection>
-        </scm>
-        <developers>
-          <developer>
-            <id>axel22</id>
-            <name>Aleksandar Prokopec</name>
-            <url>http://axel22.github.com/</url>
-          </developer>
-        </developers>,
-      mechaPublishKey <<= mechaPublishKey.dependsOn(publish),
-      mechaDocsRepoKey := "git@github.com:storm-enroute/apidocs.git",
-      mechaDocsBranchKey := "gh-pages",
-      mechaDocsPathKey := "coroutines"
-    )
+  val coroutinesSettings = MechaRepoPlugin.defaultSettings ++ Seq(
+    name := "coroutines",
+    organization := "com.storm-enroute",
+    version <<= frameworkVersion,
+    scalaVersion <<= coroutinesScalaVersion,
+    crossScalaVersions <<= coroutinesCrossScalaVersions,
+    libraryDependencies <++= (scalaVersion)(sv => dependencies(sv)),
+    libraryDependencies ++= superRepoDependencies("coroutines"),
+    testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
+    scalacOptions ++= Seq(
+      "-deprecation",
+      "-unchecked",
+      "-optimise",
+      "-Yinline-warnings"
+    ),
+    resolvers ++= Seq(
+      "Sonatype OSS Snapshots" at
+        "https://oss.sonatype.org/content/repositories/snapshots",
+      "Sonatype OSS Releases" at
+        "https://oss.sonatype.org/content/repositories/releases"
+    ),
+    ivyLoggingLevel in ThisBuild := UpdateLogging.Quiet,
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    pomExtra :=
+      <url>http://storm-enroute.com/</url>
+      <licenses>
+        <license>
+          <name>BSD-style</name>
+          <url>http://opensource.org/licenses/BSD-3-Clause</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:storm-enroute/coroutines.git</url>
+        <connection>scm:git:git@github.com:storm-enroute/coroutines.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>axel22</id>
+          <name>Aleksandar Prokopec</name>
+          <url>http://axel22.github.com/</url>
+        </developer>
+      </developers>,
+    mechaPublishKey <<= mechaPublishKey.dependsOn(publish),
+    mechaDocsRepoKey := "git@github.com:storm-enroute/apidocs.git",
+    mechaDocsBranchKey := "gh-pages",
+    mechaDocsPathKey := "coroutines"
+  )
 
   def dependencies(scalaVersion: String) =
     CrossVersion.partialVersion(scalaVersion) match {
@@ -101,121 +100,119 @@ object CoroutinesBuild extends MechaRepoBuild {
     case _ => Nil
   }
 
-  val coroutinesCommonSettings =
-    Defaults.defaultSettings ++ MechaRepoPlugin.defaultSettings ++ Seq(
-      name := "coroutines-common",
-      organization := "com.storm-enroute",
-      version <<= frameworkVersion,
-      scalaVersion <<= coroutinesScalaVersion,
-      crossScalaVersions <<= coroutinesCrossScalaVersions,
-      libraryDependencies <++= (scalaVersion)(sv => commonDependencies(sv)),
-      libraryDependencies ++= superRepoDependencies("coroutines-common"),
-      scalacOptions ++= Seq(
-        "-deprecation",
-        "-unchecked",
-        "-optimise",
-        "-Yinline-warnings"
-      ),
-      resolvers ++= Seq(
-        "Sonatype OSS Snapshots" at
-          "https://oss.sonatype.org/content/repositories/snapshots",
-        "Sonatype OSS Releases" at
-          "https://oss.sonatype.org/content/repositories/releases"
-      ),
-      ivyLoggingLevel in ThisBuild := UpdateLogging.Quiet,
-      publishMavenStyle := true,
-      publishTo <<= version { (v: String) =>
-        val nexus = "https://oss.sonatype.org/"
-        if (v.trim.endsWith("SNAPSHOT"))
-          Some("snapshots" at nexus + "content/repositories/snapshots")
-        else
-          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
-      },
-      publishArtifact in Test := false,
-      pomIncludeRepository := { _ => false },
-      pomExtra :=
-        <url>http://storm-enroute.com/</url>
-        <licenses>
-          <license>
-            <name>BSD-style</name>
-            <url>http://opensource.org/licenses/BSD-3-Clause</url>
-            <distribution>repo</distribution>
-          </license>
-        </licenses>
-        <scm>
-          <url>git@github.com:storm-enroute/coroutines.git</url>
-          <connection>scm:git:git@github.com:storm-enroute/coroutines.git</connection>
-        </scm>
-        <developers>
-          <developer>
-            <id>axel22</id>
-            <name>Aleksandar Prokopec</name>
-            <url>http://axel22.github.com/</url>
-          </developer>
-        </developers>,
-      mechaPublishKey <<= mechaPublishKey.dependsOn(publish),
-      mechaDocsRepoKey := "git@github.com:storm-enroute/apidocs.git",
-      mechaDocsBranchKey := "gh-pages",
-      mechaDocsPathKey := "coroutines-common"
-    )
+  val coroutinesCommonSettings = MechaRepoPlugin.defaultSettings ++ Seq(
+    name := "coroutines-common",
+    organization := "com.storm-enroute",
+    version <<= frameworkVersion,
+    scalaVersion <<= coroutinesScalaVersion,
+    crossScalaVersions <<= coroutinesCrossScalaVersions,
+    libraryDependencies <++= (scalaVersion)(sv => commonDependencies(sv)),
+    libraryDependencies ++= superRepoDependencies("coroutines-common"),
+    scalacOptions ++= Seq(
+      "-deprecation",
+      "-unchecked",
+      "-optimise",
+      "-Yinline-warnings"
+    ),
+    resolvers ++= Seq(
+      "Sonatype OSS Snapshots" at
+        "https://oss.sonatype.org/content/repositories/snapshots",
+      "Sonatype OSS Releases" at
+        "https://oss.sonatype.org/content/repositories/releases"
+    ),
+    ivyLoggingLevel in ThisBuild := UpdateLogging.Quiet,
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    pomExtra :=
+      <url>http://storm-enroute.com/</url>
+      <licenses>
+        <license>
+          <name>BSD-style</name>
+          <url>http://opensource.org/licenses/BSD-3-Clause</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:storm-enroute/coroutines.git</url>
+        <connection>scm:git:git@github.com:storm-enroute/coroutines.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>axel22</id>
+          <name>Aleksandar Prokopec</name>
+          <url>http://axel22.github.com/</url>
+        </developer>
+      </developers>,
+    mechaPublishKey <<= mechaPublishKey.dependsOn(publish),
+    mechaDocsRepoKey := "git@github.com:storm-enroute/apidocs.git",
+    mechaDocsBranchKey := "gh-pages",
+    mechaDocsPathKey := "coroutines-common"
+  )
 
-  val coroutinesExtraSettings =
-    Defaults.defaultSettings ++ MechaRepoPlugin.defaultSettings ++ Seq(
-      name := "coroutines-extra",
-      organization := "com.storm-enroute",
-      version <<= frameworkVersion,
-      scalaVersion <<= coroutinesScalaVersion,
-      crossScalaVersions <<= coroutinesCrossScalaVersions,
-      libraryDependencies <++= (scalaVersion)(sv => extraDependencies(sv)),
-      testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
-      scalacOptions ++= Seq(
-        "-deprecation",
-        "-unchecked",
-        "-optimise",
-        "-Yinline-warnings"
-      ),
-      resolvers ++= Seq(
-        "Sonatype OSS Snapshots" at
-          "https://oss.sonatype.org/content/repositories/snapshots",
-        "Sonatype OSS Releases" at
-          "https://oss.sonatype.org/content/repositories/releases"
-      ),
-      ivyLoggingLevel in ThisBuild := UpdateLogging.Quiet,
-      publishMavenStyle := true,
-      publishTo <<= version { (v: String) =>
-        val nexus = "https://oss.sonatype.org/"
-        if (v.trim.endsWith("SNAPSHOT"))
-          Some("snapshots" at nexus + "content/repositories/snapshots")
-        else
-          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
-      },
-      publishArtifact in Test := false,
-      pomIncludeRepository := { _ => false },
-      pomExtra :=
-        <url>http://storm-enroute.com/</url>
-        <licenses>
-          <license>
-            <name>BSD-style</name>
-            <url>http://opensource.org/licenses/BSD-3-Clause</url>
-            <distribution>repo</distribution>
-          </license>
-        </licenses>
-        <scm>
-          <url>git@github.com:storm-enroute/coroutines.git</url>
-          <connection>scm:git:git@github.com:storm-enroute/coroutines.git</connection>
-        </scm>
-        <developers>
-          <developer>
-            <id>axel22</id>
-            <name>Aleksandar Prokopec</name>
-            <url>http://axel22.github.com/</url>
-          </developer>
-        </developers>,
-      mechaPublishKey <<= mechaPublishKey.dependsOn(publish),
-      mechaDocsRepoKey := "git@github.com:storm-enroute/apidocs.git",
-      mechaDocsBranchKey := "gh-pages",
-      mechaDocsPathKey := "coroutines-extra"
-    )
+  val coroutinesExtraSettings = MechaRepoPlugin.defaultSettings ++ Seq(
+    name := "coroutines-extra",
+    organization := "com.storm-enroute",
+    version <<= frameworkVersion,
+    scalaVersion <<= coroutinesScalaVersion,
+    crossScalaVersions <<= coroutinesCrossScalaVersions,
+    libraryDependencies <++= (scalaVersion)(sv => extraDependencies(sv)),
+    testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
+    scalacOptions ++= Seq(
+      "-deprecation",
+      "-unchecked",
+      "-optimise",
+      "-Yinline-warnings"
+    ),
+    resolvers ++= Seq(
+      "Sonatype OSS Snapshots" at
+        "https://oss.sonatype.org/content/repositories/snapshots",
+      "Sonatype OSS Releases" at
+        "https://oss.sonatype.org/content/repositories/releases"
+    ),
+    ivyLoggingLevel in ThisBuild := UpdateLogging.Quiet,
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    publishArtifact in Test := false,
+    pomIncludeRepository := { _ => false },
+    pomExtra :=
+      <url>http://storm-enroute.com/</url>
+      <licenses>
+        <license>
+          <name>BSD-style</name>
+          <url>http://opensource.org/licenses/BSD-3-Clause</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:storm-enroute/coroutines.git</url>
+        <connection>scm:git:git@github.com:storm-enroute/coroutines.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>axel22</id>
+          <name>Aleksandar Prokopec</name>
+          <url>http://axel22.github.com/</url>
+        </developer>
+      </developers>,
+    mechaPublishKey <<= mechaPublishKey.dependsOn(publish),
+    mechaDocsRepoKey := "git@github.com:storm-enroute/apidocs.git",
+    mechaDocsBranchKey := "gh-pages",
+    mechaDocsPathKey := "coroutines-extra"
+  )
 
   def commonDependencies(scalaVersion: String) =
     CrossVersion.partialVersion(scalaVersion) match {
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,4 @@
+
 addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.1")
+
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
